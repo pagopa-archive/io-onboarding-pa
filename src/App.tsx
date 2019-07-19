f4da088259220cb80f97a085a984f757479c47be
d-al-ibm <davide.aliti@it.ibm.com>
2019-07-19 16:20:29 +0200

    first commit for project structure added packages needed for react, coreui, bootstrap-italia, parcel, sass

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import "./App.scss";
+
+export const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1 className="primary-bg white-color">Piattaforma Onboard PA</h1>
+    </div>
+  );
+};
