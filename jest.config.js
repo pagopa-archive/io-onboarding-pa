fc13b706b7ef9130051833bb51fc63f24fa4c6c2
d-al-ibm <davide.aliti@it.ibm.com>
2019-07-21 11:48:57 +0200

    fixed setup for testing with jest and enzyme

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "roots": [
+        "<rootDir>/src"
+    ],
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "node"
+    ],
+    "testEnvironment": "node",
+
+    // Setup Enzyme
+    "snapshotSerializers": ["enzyme-to-json/serializer"],
+    "setupTestFrameworkScriptFile": "<rootDir>/src/setupEnzyme.ts",
+    "moduleNameMapper": {
+        "^.+\\.(css|less|scss)$": "babel-jest"
+    }
+}
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,8 @@
+import { shallow } from "enzyme";
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 import { App } from "../App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("mounts without crashing", () => {
+  const wrapper = shallow(<App />);
+  wrapper.unmount();
 });
