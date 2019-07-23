d9859845adec96905f8273c5b3797577c4105126
d-al-ibm <davide.aliti@it.ibm.com>
2019-07-23 10:37:49 +0200

    fixed package.json to include latest versions of packages fixed tsconfig and jest.config for problems with tests moved setupEnzyme to root folder create styleMocks to mock style files for test removed index.ts that created problems with tests

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-    "roots": [
-        "<rootDir>/src"
-    ],
-    "transform": {
-        "^.+\\.tsx?$": "ts-jest"
-    },
-    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-    "moduleFileExtensions": [
-        "ts",
-        "tsx",
-        "js",
-        "jsx",
-        "json",
-        "node"
-    ],
-    "testEnvironment": "node",
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "testEnvironment": "jsdom",
 
-    // Setup Enzyme
-    "snapshotSerializers": ["enzyme-to-json/serializer"],
-    "setupTestFrameworkScriptFile": "<rootDir>/src/setupEnzyme.ts",
-    "moduleNameMapper": {
-        "^.+\\.(css|less|scss)$": "babel-jest"
-    }
-}
+  // Setup Enzyme
+  "snapshotSerializers": ["enzyme-to-json/serializer"],
+  "setupFilesAfterEnv": ["<rootDir>/setupEnzyme.ts"],
+  "moduleNameMapper": {
+    ".+\\.(css|scss)$": "<rootDir>/styleMock.ts"
+  },
+};
