895e36c3c1eaf39047c7196f3057dd078172e370
d-al-ibm <davide.aliti@it.ibm.com>
2019-07-31 17:23:03 +0200

    fixed imports and comments for ts-lint

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 
-import "./Home.css";
 import ioLogoWhite from "../../assets/img/io-logo-white.svg";
 import mockImage from "../../assets/img/mock-img-white.png";
+import "./Home.css";
 
 export const Home: React.FC = () => {
   return (
@@ -64,11 +64,11 @@ export const Home: React.FC = () => {
         </div>
         <div className="row justify-content-start home-row-logos">
           <div className="col-6 text-right">
-            /* TODO: fix with right images when available - keep track with story 167623550 */
+            {/*TODO: fix with right images when available - keep track with story 167623550*/}
             <img src={mockImage} width={220} />
           </div>
           <div className="col-6 text-left">
-            /* TODO: fix with right images when available - keep track with story 167623550 */
+            {/* TODO: fix with right images when available - keep track with story 167623550*/}
             <img src={mockImage} width={220} />
           </div>
         </div>
