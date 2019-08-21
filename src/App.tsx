85307a352a63730b8468f5b1460e04e17042651b
d-al-ibm <davide.aliti@it.ibm.com>
2019-08-21 12:13:43 +0200

    fixed router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { DefaultContainer } from "./containers/DefaultContainer/DefaultContainer";
 import { Home } from "./containers/Home/Home";
 import { LoadingScreen } from "./containers/LoadingScreen/LoadingScreen";
@@ -11,12 +11,15 @@ export const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div className="App h-100">
-        <Route path="/" exact component={Home} />
-        <Route
-          path="/(generazione-documenti|conferma-documenti)"
-          component={LoadingScreen}
-        />
-        <Route path="/*" component={DefaultContainer} />
+        <Switch>
+          <Route exact path="/" component={() => <Redirect to="/home" />} />
+          <Route path="/home" component={Home} />
+          <Route
+            path="/(generazione-documenti|conferma-documenti)"
+            component={LoadingScreen}
+          />
+          <Route path="/*" component={DefaultContainer} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
