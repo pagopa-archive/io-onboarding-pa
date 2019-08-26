444d80fe8b1fbeab120a60fc00236b765ebb6117
d-al-ibm <davide.aliti@it.ibm.com>
2019-08-27 01:01:40 +0200

    added typeahed

--- src/containers/RegistrationSteps/RegistrationStepOne/RegistrationStepOne.tsx
+++ src/containers/RegistrationSteps/RegistrationStepOne/RegistrationStepOne.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { RouteComponentProps, withRouter } from "react-router";
+import { AsyncTypeahead } from "react-bootstrap-typeahead";
 
 interface IRegistrationStepOneCustomProps {
   institution: {
@@ -25,10 +26,30 @@ export const RegistrationStepOne = withRouter<
   IRegistrationStepOneProps,
   React.FC<IRegistrationStepOneProps>
 >((props: IRegistrationStepOneProps) => {
+  const options: ReadonlyArray<{ login: string }> = [
+    { login: "ACAC" },
+    { login: "B" },
+    { login: "CLLL" }
+  ];
+
+  const handleSearch = (query: string) => {
+    console.log(query);
+  };
+
   return (
     <div className="RegistrationStepOne">
-      {props.institution.institutionName} - {props.match.params.registrationStep}
-
+      {props.institution.institutionName} -{" "}
+      {props.match.params.registrationStep}
+      <AsyncTypeahead
+        allowNew={false}
+        isLoading={false}
+        multiple={false}
+        options={options}
+        labelKey="login"
+        minLength={3}
+        onSearch={handleSearch}
+        placeholder="Search for a string..."
+      />
     </div>
   );
 });
--- src/customDeclarations.d.ts
+++ src/customDeclarations.d.ts
@@ -1,3 +1,4 @@
 declare module "*.png";
 declare module "*.svg";
 declare module "@coreui/react";
+declare module "react-bootstrap-typeahead";
