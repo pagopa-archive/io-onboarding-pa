637b8b34e6daedb42defba09bf806aec4cceecb2
d-al-ibm <davide.aliti@it.ibm.com>
2019-08-27 16:49:50 +0200

    added env + test variable

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ export const App: React.FC = () => {
   return (
     <div className="App">
       <h1 className="primary-bg white-color">Piattaforma Onboard PA</h1>
+      <h1 className="primary-bg white-color">{ process.env.REACT_APP_NOT_SECRET_CODE }</h1>
     </div>
   );
 };
