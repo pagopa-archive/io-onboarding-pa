ee18ba54a0ebe255021b2ff84d1a5f1c976ee7a8
d-al-ibm <davide.aliti@it.ibm.com>
2019-09-12 13:10:14 +0200

    [#168297427 - #168297370 - #168297310] completed registration step one component

--- src/components/Registration/RegistrationStepOne/RegistrationStepOne.tsx
+++ src/components/Registration/RegistrationStepOne/RegistrationStepOne.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import { index } from "fp-ts/lib/Array";
+import React, { ComponentProps } from "react";
 import { RouteComponentProps, withRouter } from "react-router";
 import {
   Button,
   Col,
+  Container,
   Form,
   FormGroup,
   FormText,
@@ -12,150 +14,168 @@ import {
 } from "reactstrap";
 import { SearchInstitutions } from "./SearchInstitutions";
 
-interface IRegistrationStepOneProps {
-  institution: {
-    institutionName: string;
-    institutionFiscalCode: string;
-    institutionAdminName: string;
-    institutionPecs: ReadonlyArray<string>;
-    institutionLegalRepName: string;
-    institutionLegalRepSurname: string;
-    institutionLegalRepCf: string;
-  };
-  onInstitutionSelected: (
-    event: ReadonlyArray<{
-      institutionName: string;
-      institutionFiscalCode: string;
-      institutionAdminName: string;
-      institutionPecs: ReadonlyArray<string>;
-      institutionLegalRepName: string;
-      institutionLegalRepSurname: string;
-      institutionLegalRepCf: string;
-    }>
-  ) => void;
-}
-
 interface IPathParams {
   registrationStep: string;
 }
 
 interface IRegistrationStepOneProps
-  extends IRegistrationStepOneCustomProps,
+  extends ComponentProps<typeof SearchInstitutions>,
     RouteComponentProps<IPathParams> {}
 
-export const RegistrationStepOne = withRouter<
-  IRegistrationStepOneProps,
-  React.FC<IRegistrationStepOneProps>
-  >((props: IRegistrationStepOneProps) => {
-  const pecRadioButtons = props.institution.institutionPecs.map(pec => {
-    return (
-      <FormGroup check className="radio" key={pec}>
-        <Input
-          className="form-check-input"
-          type="radio"
-          id={`radio-${pec}`}
-          name="radios-pec"
-          value={`radio-${pec}`}
-        />
-        <Label check className="form-check-label" htmlFor={`radio-${pec}`}>
-          {pec}
-        </Label>
-      </FormGroup>
+/**
+ * Component for institution search with autocomplete
+ */
+export const RegistrationStepOne = withRouter(
+  (props: IRegistrationStepOneProps) => {
+    const pecRadioButtons = props.selectedInstitution.pecs.map(
+      (pec, pecIndex) => {
+        return (
+          <FormGroup check className="radio" key={pecIndex}>
+            <Input
+              className="form-check-input"
+              type="radio"
+              id={`radio-${pec}`}
+              name="radios-pec"
+              value={pecIndex}
+              checked={props.selectedInstitution.selectedPecIndex === pecIndex}
+              onChange={() => {
+                props.selectedInstitution.selectedPecIndex = pecIndex;
+              }}
+            />
+            <Label check className="form-check-label" htmlFor={`radio-${pec}`}>
+              {pec}
+            </Label>
+          </FormGroup>
+        );
+      }
     );
-  });
 
-  return (
-    <div className="RegistrationStepOne">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-10">
-            <h1 className="pt-5">Ricerca Ente</h1>
-            <div className="row pt-4">
-              <div className="col">
-                <SearchInstitutions
-                  onInstitutionSelected={props.onInstitutionSelected}
-                />
-                <Form
-                  action=""
-                  method="post"
-                  encType="multipart/form-data"
-                  className="form-horizontal w-100 pt-5"
-                >
-                  <FormGroup row className="pt-5">
-                    <Col xs="12">
-                      <Label htmlFor="ipa-code-input" className="active">
-                        Codice Fiscale
-                      </Label>
-                      <Input
-                        type="text"
-                        id="ipa-code-input"
-                        name="ipa-code-input"
-                        placeholder=""
-                        readOnly
-                        value={props.institution.institutionFiscalCode || ""}
-                      />
-                      <FormText color="muted">
-                        Precompilato da IndicePA
-                      </FormText>
+    const institutionScopes: ReadonlyArray<{ label: string; value: string }> = [
+      { label: "Locale (es. comuni, regioni)", value: "LOCAL" },
+      { label: "Nazionale (es. enti centrali)", value: "NATIONAL" }
+    ];
+
+    const scopeRadioButtons = institutionScopes.map(scope => {
+      return (
+        <FormGroup check className="radio" key={scope.value}>
+          <Input
+            className="form-check-input"
+            type="radio"
+            id={`radio-${scope.value}`}
+            name="radios-scopes"
+            value={scope.value}
+            checked={props.selectedInstitution.scope === scope.value}
+            onChange={() => {
+              props.selectedInstitution.scope = scope.value;
+            }}
+          />
+          <Label
+            check
+            className="form-check-label"
+            htmlFor={`radio-${scope.value}`}
+          >
+            {scope.label}
+          </Label>
+        </FormGroup>
+      );
+    });
+
+    return (
+      <div className="RegistrationStepOne">
+        <Container fluid>
+          <Row>
+            <Col sm="10">
+              <h1 className="pt-5">Ricerca Ente</h1>
+              <Row className="pt-4">
+                <Col>
+                  <SearchInstitutions
+                    selectedInstitution={props.selectedInstitution}
+                    onInstitutionSelected={props.onInstitutionSelected}
+                  />
+                  <Form
+                    action=""
+                    method="post"
+                    encType="multipart/form-data"
+                    className="form-horizontal w-100 pt-5"
+                  >
+                    <FormGroup row className="pt-5">
+                      <Col sm="12">
+                        <Label htmlFor="ipa-code-input" className="active">
+                          Codice Fiscale*
+                        </Label>
+                        <Input
+                          type="text"
+                          id="ipa-code-input"
+                          name="ipa-code-input"
+                          placeholder=""
+                          readOnly
+                          value={props.selectedInstitution.fiscalCode || ""}
+                        />
+                        <FormText color="muted">
+                          *Precompilato da IndicePA
+                        </FormText>
+                      </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                      <Col md="3">
+                        <Label htmlFor="admin-name-input">
+                          Nome Amministrazione*
+                        </Label>
+                      </Col>
+                      <Col sm="12">
+                        <Input
+                          type="text"
+                          id="admin-name-input"
+                          name="admin-name-input"
+                          placeholder=""
+                          readOnly
+                          value={props.selectedInstitution.name || ""}
+                        />
+                        <FormText color="muted">
+                          *Precompilato da IndicePA
+                        </FormText>
+                      </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                      <Col sm="3">
+                        <Label htmlFor="text-input">Indirizzo PEC</Label>
+                      </Col>
+                      <Col sm="9">{pecRadioButtons}</Col>
+                    </FormGroup>
+                    <FormGroup row>
+                      <Col sm="3">
+                        <Label htmlFor="text-input">Area di competenza</Label>
+                      </Col>
+                      <Col sm="9">{scopeRadioButtons}</Col>
+                    </FormGroup>
+                  </Form>
+                  <Row>
+                    <Col size={6} className="text-left">
+                      <Button
+                        outline
+                        color="secondary"
+                        className="w-50"
+                        onClick={() => props.history.push("/dashboard/")}
+                      >
+                        Annulla
+                      </Button>
                     </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="admin-name-input">
-                        Nome Amministratore
-                      </Label>
+                    <Col size={6} className="text-right">
+                      <Button
+                        color="primary"
+                        className="w-50"
+                        onClick={() => props.history.push("/registrazione/2")}
+                      >
+                        Conferma
+                      </Button>
                     </Col>
-                    <Col xs="12" md="9">
-                      <Input
-                        type="text"
-                        id="admin-name-input"
-                        name="admin-name-input"
-                        placeholder=""
-                        readOnly
-                        value={props.institution.institutionAdminName || ""}
-                      />
-                      <FormText color="muted">
-                        Precompilato da IndicePA
-                      </FormText>
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="text-input">Indirizzo PEC</Label>
-                    </Col>
-                    <Col md="9">{pecRadioButtons}</Col>
-                  </FormGroup>
-                </Form>
-                <Row>
-                  <Col size={6} className="text-left">
-                    <Button
-                      outline
-                      color="secondary"
-                      className="w-50"
-                      onClick={() => props.history.push("/dashboard/")}
-                    >
-                      Annulla
-                    </Button>
-                  </Col>
-                  <Col size={6} className="text-right">
-                    <Button
-                      color="primary"
-                      className="w-50"
-                      onClick={() => props.history.push("/registrazione/2")}
-                    >
-                      Conferma
-                    </Button>
-                  </Col>
-                </Row>
-              </div>
-            </div>
-          </div>
-          <div className="col-2"></div>
-        </div>
+                  </Row>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
       </div>
-
-      {/*{props.institution.institutionName} -{" "}*/}
-      {/*{props.match.params.registrationStep}*/}
-    </div>
-  );
-});
+    );
+  }
+);
