334f2d345bda652ab71eb7512f42fd838fc839ee
d-al-ibm <davide.aliti@it.ibm.com>
2019-09-18 16:48:34 +0200

    [#168579686] created dummy button to go to sign up page

--- src/components/DelegateDashboard/DelegateDashboard.tsx
+++ src/components/DelegateDashboard/DelegateDashboard.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { withRouter } from "react-router";
+import { Button, Col, Row } from "reactstrap";
 
 /**
  * Component for delegate dashboard
  */
-export const DelegateDashboard = () => {
-  return <div>Dashboard!</div>;
-};
+export const DelegateDashboard = withRouter(props => {
+  return (
+    <div className="Dashboard">
+      <Row>
+        <Col className="text-center">
+          <Button
+            color="danger"
+            onClick={() => {
+              return props.history.push("sign-up/1");
+            }}
+          >
+            DUMMY BUTTON REGISTRATION
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  );
+});
