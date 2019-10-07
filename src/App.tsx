61c8b19d685c0531588dcc2243e7fa48675e5fab
d-al-ibm <52445380+d-al-ibm@users.noreply.github.com>
2019-10-07 11:06:46 +0200

    [#168930666] Add HOC to scroll to top (#35)

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { LoadingScreen } from "./components/LoadingScreen/LoadingScreen";
 
 import "../node_modules/bootstrap-italia/dist/css/bootstrap-italia.min.css";
 import "./App.scss";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 /**
  * Entry point for app, with first level routing
@@ -13,14 +14,19 @@ import "./App.scss";
 export const App = () => {
   return (
     <BrowserRouter>
-      <div className="App vh-100">
-        <Switch>
-          <Route exact path="/" component={() => <Redirect to="/home" />} />
-          <Route path="/home" component={Home} />
-          <Route path="/(creating-docs|docs-sent)" component={LoadingScreen} />
-          <Route path="/*" component={DefaultContainer} />
-        </Switch>
-      </div>
+      <ScrollToTop>
+        <div className="App vh-100">
+          <Switch>
+            <Route exact path="/" component={() => <Redirect to="/home" />} />
+            <Route path="/home" component={Home} />
+            <Route
+              path="/(creating-docs|docs-sent)"
+              component={LoadingScreen}
+            />
+            <Route path="/*" component={DefaultContainer} />
+          </Switch>
+        </div>
+      </ScrollToTop>
     </BrowserRouter>
   );
 };
