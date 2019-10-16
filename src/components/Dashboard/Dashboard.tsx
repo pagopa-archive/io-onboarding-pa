ff4a566419cb89058f2a3ccdde9642a588393667
d-al-ibm <52445380+d-al-ibm@users.noreply.github.com>
2019-10-16 15:19:33 +0200

    [#169127770] Profile integration (#41)

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,13 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { withRouter } from "react-router";
 import { Button, Col, Row } from "reactstrap";
 import { TokenContext } from "../../context/token-context";
+import { ICustomWindow } from "../../customTypes/CustomWindow";
 
 /**
  * Component for delegate dashboard
  */
 export const Dashboard = withRouter(props => {
   const tokenContext = useContext(TokenContext);
+
+  /**
+   * Create window with custom element _env_ for environment variables
+   */
+  const customWindow = window as ICustomWindow;
 
   /**
    * Given a cookie key `cookieName`, returns the value of
@@ -32,9 +38,37 @@ export const Dashboard = withRouter(props => {
   /**
    * Set token in token context
    */
-  tokenContext.setToken(getCookie("sessionToken"));
+  useEffect(() => {
+    /**
+     * Set token in token context
+     */
+    tokenContext.setToken(getCookie("sessionToken"));
+  }, []);
 
-  console.log("setToken called!", tokenContext.token);
+  useEffect(() => {
+    if (tokenContext.token) {
+      const url =
+        customWindow._env_.IO_ONBOARDING_PA_API_HOST +
+        ":" +
+        customWindow._env_.IO_ONBOARDING_PA_API_PORT +
+        "/profile";
+      fetch(url, {
+        headers: {
+          Accept: "application/json",
+          Authorization: "Bearer " + tokenContext.token
+          // 'Content-Type': 'application/json'
+        },
+        method: "GET"
+      })
+        .then(response => {
+          return response.json();
+        })
+        .then(responseData => {
+          // TODO: add code to manage get profile response, tracked with stories #169127957 and #168752398
+          console.log(responseData);
+        });
+    }
+  }, [tokenContext.token]);
 
   return (
     <div className="Dashboard">
