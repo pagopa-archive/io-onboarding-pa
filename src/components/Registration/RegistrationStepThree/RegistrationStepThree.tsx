87de04e3956f0ddd5af618945ff7a832584aabd5
d-al-ibm <davide.aliti@it.ibm.com>
2019-11-14 17:01:35 +0100

    [#169647056] sending documents for institution registration added api call to send documents for institution registration at sign un step 3 confirmation added translations

--- src/components/Registration/RegistrationStepThree/RegistrationStepThree.tsx
+++ src/components/Registration/RegistrationStepThree/RegistrationStepThree.tsx
@@ -1,7 +1,8 @@
 import * as FileSaver from "file-saver";
-import React, { ComponentProps, Fragment, MouseEvent } from "react";
+import React, { ComponentProps, Fragment, MouseEvent, useContext } from "react";
 import { useCookies } from "react-cookie";
 import { useTranslation } from "react-i18next";
+import { RouteComponentProps, withRouter } from "react-router-dom";
 import {
   Button,
   Col,
@@ -14,10 +15,11 @@ import {
   Row
 } from "reactstrap";
 import logoSignupStepThree from "../../../assets/img/signup_step3.svg";
+import { AlertContext } from "../../../context/alert-context";
 import { ICustomWindow } from "../../../customTypes/CustomWindow";
 import { SearchAdministrations } from "../RegistrationStepOne/SearchAdministrations";
 
-interface IRegistrationStepThreeProps {
+interface IRegistrationStepThreeProps extends RouteComponentProps {
   selectedAdministration: ComponentProps<
     typeof SearchAdministrations
   >["selectedAdministration"];
@@ -30,10 +32,15 @@ interface IRegistrationStepThreeProps {
  * Create sections to download documents
  */
 
-interface IDocumentDownloadSectionProps {
-  ipaCode: string;
+interface IDocumentInfo {
   documentType: string;
   documentName: string;
+}
+
+interface IDocumentDownloadSectionProps extends IDocumentInfo {
+  ipaCode: string;
+  cookie: string;
+  urlDomainPort: string;
 }
 
 const DownloadDocsSection = (props: IDocumentDownloadSectionProps) => {
@@ -42,26 +49,14 @@ const DownloadDocsSection = (props: IDocumentDownloadSectionProps) => {
    */
   const { t } = useTranslation();
 
-  /**
-   * Create window with custom element _env_ for environment variables
-   */
-  const customWindow = (window as unknown) as ICustomWindow;
-
-  const urlDomainPort =
-    customWindow._env_.IO_ONBOARDING_PA_API_HOST +
-    ":" +
-    customWindow._env_.IO_ONBOARDING_PA_API_PORT;
-
-  const [cookies] = useCookies(["sessionToken"]);
-
   const downloadDocument = () => (_: MouseEvent) => {
     const url =
-      urlDomainPort +
+      props.urlDomainPort +
       `/organizations/${props.ipaCode}/documents/${props.documentName}`;
     // TODO: use generated classes for api (tracked in story https://www.pivotaltracker.com/story/show/169454440)
     fetch(url, {
       headers: {
-        Authorization: `Bearer ${cookies.sessionToken}`
+        Authorization: `Bearer ${props.cookie}`
       },
       method: "GET"
     })
@@ -96,103 +91,140 @@ const DownloadDocsSection = (props: IDocumentDownloadSectionProps) => {
 /**
  * Component for third step of registration process
  */
-export const RegistrationStepThree = (props: IRegistrationStepThreeProps) => {
-  /**
-   * react-i18next translation hook
-   */
-  const { t } = useTranslation();
+export const RegistrationStepThree = withRouter(
+  (props: IRegistrationStepThreeProps) => {
+    /**
+     * react-i18next translation hook
+     */
+    const { t } = useTranslation();
 
-  /**
-   * array containing two documents download sections props
-   */
-  const downloadDocsSectionsDataArray: ReadonlyArray<ComponentProps<
-    typeof DownloadDocsSection
-  >> = [
-    {
-      documentName: "contract.pdf",
-      documentType: "contract",
-      ipaCode: props.selectedAdministration.ipa_code
-    },
-    {
-      documentName: `mandate-${props.userFiscalCode.toLowerCase()}.pdf`,
-      documentType: "mandate",
-      ipaCode: props.selectedAdministration.ipa_code
-    }
-  ];
+    /**
+     * Create window with custom element _env_ for environment variables
+     */
+    const customWindow = (window as unknown) as ICustomWindow;
 
-  const downloadDocsSections = downloadDocsSectionsDataArray.map(
-    downloadDocSection => (
-      <DownloadDocsSection
-        key={downloadDocSection.documentType}
-        ipaCode={downloadDocSection.ipaCode}
-        documentType={downloadDocSection.documentType}
-        documentName={downloadDocSection.documentName}
-      />
-    )
-  );
+    const urlDomainPort =
+      customWindow._env_.IO_ONBOARDING_PA_API_HOST +
+      ":" +
+      customWindow._env_.IO_ONBOARDING_PA_API_PORT;
 
-  return (
-    <div className="RegistrationStepThree">
-      <Container fluid={true}>
-        <Row>
-          <Col sm="10">
-            <Row>
-              <Col sm="11">
-                <h1 className="pt-5">{t("signUp.stepThree.title")}</h1>
-                <Row className="pt-3">
-                  <Col>
-                    <p>{t("signUp.stepThree.description")}</p>
-                  </Col>
-                </Row>
-                <Row className="pt-2">
-                  <Col>
-                    <p>{t("signUp.stepThree.additionalDescription")}</p>
-                  </Col>
-                </Row>
-                {downloadDocsSections}
-                <Form className="pt-5">
-                  <FormGroup check={true}>
-                    <Input
-                      type="checkbox"
-                      name="document-confirm-check"
-                      id="documentConfirm"
-                      checked={props.isViewedDocumentsCheckboxChecked}
-                      onChange={props.onIsViewedDocumentsCheckboxChanged}
-                    />
-                    <Label for="documentConfirm" check={true}>
-                      {t("signUp.stepThree.checkboxLabel")}
-                    </Label>
-                  </FormGroup>
-                </Form>
-                <Row className="pt-5 pb-5">
-                  <Col size={6} className="text-left">
-                    <Button outline={true} color="secondary" className="w-50">
-                      {t("common.buttons.cancel")}
-                    </Button>
-                  </Col>
-                  <Col size={6} className="text-right">
-                    <Button
-                      color="primary"
-                      className="w-50"
-                      disabled={!props.isViewedDocumentsCheckboxChecked}
-                    >
-                      {t("common.buttons.confirm")}
-                    </Button>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Col>
-          <Col sm="2">
-            <Media
-              object={true}
-              src={logoSignupStepThree}
-              alt="Signup step one logo"
-              className="pt-5 w-75"
-            />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
-};
+    const [cookies] = useCookies(["sessionToken"]);
+
+    const alertContext = useContext(AlertContext);
+
+    /**
+     * array containing two documents download sections props
+     */
+    const downloadDocsSectionsDataArray: ReadonlyArray<IDocumentInfo> = [
+      {
+        documentName: "contract.pdf",
+        documentType: "contract"
+      },
+      {
+        documentName: `mandate-${props.userFiscalCode.toLowerCase()}.pdf`,
+        documentType: "mandate"
+      }
+    ];
+
+    const downloadDocsSections = downloadDocsSectionsDataArray.map(
+      downloadDocSection => (
+        <DownloadDocsSection
+          key={downloadDocSection.documentType}
+          documentType={downloadDocSection.documentType}
+          documentName={downloadDocSection.documentName}
+          ipaCode={props.selectedAdministration.ipa_code}
+          cookie={cookies.sessionToken}
+          urlDomainPort={urlDomainPort}
+        />
+      )
+    );
+
+    const onSendDocuments = () => {
+      const url =
+        urlDomainPort +
+        `/organizations/${props.selectedAdministration.ipa_code}/signed-documents`;
+      // TODO: use generated classes for api (tracked in story https://www.pivotaltracker.com/story/show/169454440)
+      fetch(url, {
+        headers: {
+          Authorization: `Bearer ${cookies.sessionToken}`
+        },
+        method: "POST"
+      })
+        .then(_ => {
+          props.history.push("/dashboard");
+          alertContext.setAlert({
+            alertColor: "warning",
+            alertText: t("common.alerts.documentsSent"),
+            showAlert: true
+          });
+        })
+        .catch(error => error);
+    };
+
+    return (
+      <div className="RegistrationStepThree">
+        <Container fluid={true}>
+          <Row>
+            <Col sm="10">
+              <Row>
+                <Col sm="11">
+                  <h1 className="pt-5">{t("signUp.stepThree.title")}</h1>
+                  <Row className="pt-3">
+                    <Col>
+                      <p>{t("signUp.stepThree.description")}</p>
+                    </Col>
+                  </Row>
+                  <Row className="pt-2">
+                    <Col>
+                      <p>{t("signUp.stepThree.additionalDescription")}</p>
+                    </Col>
+                  </Row>
+                  {downloadDocsSections}
+                  <Form className="pt-5">
+                    <FormGroup check={true}>
+                      <Input
+                        type="checkbox"
+                        name="document-confirm-check"
+                        id="documentConfirm"
+                        checked={props.isViewedDocumentsCheckboxChecked}
+                        onChange={props.onIsViewedDocumentsCheckboxChanged}
+                      />
+                      <Label for="documentConfirm" check={true}>
+                        {t("signUp.stepThree.checkboxLabel")}
+                      </Label>
+                    </FormGroup>
+                  </Form>
+                  <Row className="pt-5 pb-5">
+                    <Col size={6} className="text-left">
+                      <Button outline={true} color="secondary" className="w-50">
+                        {t("common.buttons.cancel")}
+                      </Button>
+                    </Col>
+                    <Col size={6} className="text-right">
+                      <Button
+                        color="primary"
+                        className="w-50"
+                        disabled={!props.isViewedDocumentsCheckboxChecked}
+                        onClick={onSendDocuments}
+                      >
+                        {t("common.buttons.confirm")}
+                      </Button>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Col>
+            <Col sm="2">
+              <Media
+                object={true}
+                src={logoSignupStepThree}
+                alt="Signup step one logo"
+                className="pt-5 w-75"
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+);
