d316a28d5a34886bc98c55b2c94dcf1f5e7aaf7d
Danilo Spinelli <gunzip@users.noreply.github.com>
2019-12-02 09:28:04 +0100

    Update src/components/Dashboard/Dashboard.tsx

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -51,7 +51,7 @@ const OrganizationsRow = (props: IOrganizationRowProps) => {
             <a
               target="_blank"
               rel="noopener noreferrer"
-              href="http://www.indicepa.gov.it"
+              href="https://www.indicepa.gov.it"
             >
               {t("common.links.ipa.link")}
             </a>
